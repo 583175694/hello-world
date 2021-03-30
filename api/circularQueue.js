293f67eec5d5e3ce5bf8ed17a2542daa266996ba
Hang <583175694@qq.com>
2021-03-30 20:39:03 +0800

    base

---
+++ api/circularQueue.js
@@ -0,0 +1,37 @@
+/**
+ * @循环队列
+ * */
+
+class CircularQueue {
+  constructor(n) {
+    this.items = new Array()
+    this.n = n
+    this.head = 0
+    this.tail = 0
+  }
+
+  enquene(item) {
+    if ((this.tail + 1) % this.n === this.head) return false
+    this.items[this.tail] = item
+    this.tail = (this.tail + 1) % this.n
+    return true
+  }
+
+  dequene() {
+    if (this.tail === this.head) return null
+    const ret = this.items[this.head]
+    this.head = (this.head + 1) % n
+    return ret
+  }
+}
+
+
+const queue = new CircularQueue(3)
+
+console.log(queue.enqueue(1), queue)
+console.log(queue.enqueue(2), queue)
+console.log(queue.enqueue(3), queue)
+console.log(queue.dequeue(), queue)
+console.log(queue.enqueue(4), queue)
+console.log(queue.dequeue(), queue)
+console.log(queue.enqueue(5), queue)
